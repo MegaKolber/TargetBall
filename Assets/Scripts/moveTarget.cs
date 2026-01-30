49540619bd331f1c14bf68428171813524f181ee
jbagh2 <jbagheri2@huskers.unl.edu>
2026-01-30 16:43:30 -0600

    movingTarget prefab made

---
+++ Assets/Scripts/moveTarget.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private double x_min = -3.5;
+    private double x_max = 3.7;
+    private double y_min = 3.5;
+    private double y_max = 8;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
